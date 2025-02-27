7aefbbadde42dbaefc69b5a92888eb7c2b095f92
Claudio Santini <hireclaudio@gmail.com>
2025-02-27 19:29:45 +0100

    TestCase

---
+++ tests/test_TestCase.py
@@ -0,0 +1,22 @@
+import unittest
+from unitai import TestCase as unitai_TestCase
+
+
+class TestTestCase(unittest.TestCase):
+    """Some inception going on here"""
+
+    def test_wraps_and_count(self):
+        tc = unitai_TestCase()
+        tc.assertEqual(1, 1)
+        tc.assertTrue(True)
+        tc.assertGreater(2, 1)
+        try:
+            tc.assertEqual(1, 0)
+        except Exception:
+            pass
+
+        try:
+            tc.assertFalse(True)
+        except Exception:
+            pass
+        self.assertEqual(tc._successes, 3)
